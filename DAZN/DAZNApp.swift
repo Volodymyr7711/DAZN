2a1884a8c7ef9f94859fc5efecb6d32b8097ceb7
Volodymyr Mendyk <volodymyrmendyk70@gmail.com>
2023-05-12 16:25:25 +0200

    Initial Commit

---
+++ DAZN/DAZNApp.swift
@@ -0,0 +1,17 @@
+//
+//  DAZNApp.swift
+//  DAZN
+//
+//  Created by Volodymyr Mendyk on 12/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct DAZNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
